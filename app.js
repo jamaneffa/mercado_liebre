535e39a28ed00b20429fd492fc222dca523785ff
Juan Maneffa <106563046+jamaneffa@users.noreply.github.com>
2022-07-20 12:49:07 -0300

    Modifique configuracion de puerto

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 const publicPath = path.resolve(__dirname,'./public')
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log("Servidor en linea"))
 
